b344ef81c914afd2430485234c18ef872ec939bc
yuanxun wang <wangyuanxun@smail.nju.edu.cn>
2017-11-17 20:24:19 +0800

    task1
     task1 complete

---
+++ svm/task1/load_images.m
@@ -0,0 +1,21 @@
+function [set, labels] = load_images(filename)
+
+% initialization
+img_dir = dir([filename, '*.jpg']);
+set = zeros(224, 224, 3, set_num);
+labels = zeros(set_num, 1);
+
+% read the image
+parfor i = 1:set_num
+    image = imread([filename, img_dir(i).name]);
+    if ismatrix(image)  
+        image = cat(3,image,image,image);  
+    end
+    image = imresize(image, [224, 224]);
+    name_split = regexp(img_dir(i).name, '_', 'split');
+    label = cell2mat(name_split(2));
+    set(:, :, :, i) = reshape(image, 1, 224, 224, 3);
+    labels(i, 1) = str2double(label);
+end
+
+end
---
+++ svm/task1/svm.m
@@ -0,0 +1,27 @@
+clc;
+
+% enable parallel computing
+cluster = parcluster('local');
+cluster.num_of_workers = 4;
+parpool(cluster, cluster.num_of_workers);
+
+% read the data, dividing it into training set and test set
+[training_set, training_labels] = load_images('./IMDB_WIKI/train_imdb_data/', 800);
+[test_set, test_labels] = load_images('./IMDB_WIKI/test_imdb_data/', 200);
+
+% extract the feature using cnn
+vgg_neural_network = vgg16;
+layer = 'fc7';
+training_features = activations(vgg_neural_network, training_set, layer);
+test_features = activations(vgg_neural_network, test_set, layer);
+clear training_set;
+clear test_set;
+
+% use PCA to reduct dimension
+% eigen_vector = pca(training_features);
+% training_features = training_features * eigen_vector(:, 1:1000);
+% test_features = training_features * eigen_vector(:, 1:1000);
+
+% classfy the test data(male and female) using libsvm
+svm_model = svmtrain(training_labels, double(training_features), '-t 0');
+accuracy = svmpredict(test_labels, double(test_features), svm_model);
