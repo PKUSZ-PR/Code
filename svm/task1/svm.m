c92184f31f15d1ed6a83283d57534ec820430f1c
yuanxun wang <wangyuanxun@smail.nju.edu.cn>
2017-11-28 20:06:02 +0800

    bug fix
    bug fix

--- svm/task1/svm.m
+++ svm/task1/svm.m
@@ -8,7 +8,7 @@ if isempty(gcp('nocreate'))
 end
 
 % read the data, dividing it into training set and test set
-[set, labels] = load_images('./IMDB_WIKI/imdb_data/', 2000);
+[set, labels] = load_images('./IMDB_WIKI/imdb_data/', 1000);
 
 % extract the feature using cnn
 alex_neural_network = alexnet;
@@ -17,7 +17,7 @@ set = activations(alex_neural_network, set, layer);
 
 % use PCA to reduct dimension
 eigen_vector = pca(set);
-training_set = set * eigen_vector(:, 1:4096);
+training_set = set * eigen_vector(:, 1:700);
 
 % classfy the test data(male and female) using libsvm and 10-fold validation
 svm_model = svmtrain(labels, double(training_set), '-v 10');
